0e8578bfebce3aee6bea8046cc78b000d31ca689
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-04-05 22:15:54 -0300

    Más casos de test

--- tests.cpp
+++ tests.cpp
@@ -19,5 +19,28 @@ TEST(TestFuerzaBruta, NoHaySolucion)
 	vector<pair<int,int>> v = {x1, x2, x3};
 
 	EXPECT_EQ(solucionFuerzaBruta(v, 2), 0);
-	
+}
+
+TEST(TestFuerzaBruta, HaySolucion)
+{
+	pair<int,int> x5(6, 8);
+	pair<int,int> x3(5, 10);
+	pair<int,int> x4(7, 7);
+	pair<int,int> x2(4, 1);
+	pair<int,int> x1(3, 9);
+	vector<pair<int,int>> v = {x5, x3, x4, x2, x1};
+
+	EXPECT_EQ(solucionFuerzaBruta(v, 7), 10);
+	EXPECT_EQ(solucionFuerzaBruta(v, 18), 28);
+	EXPECT_EQ(solucionFuerzaBruta(v, 25), 35);
+
+	pair<int,int> x6(18,28);
+	v.push_back(x6);
+	EXPECT_EQ(solucionFuerzaBruta(v, 18), 28);
+	v.pop_back();
+
+	pair<int,int> x7(25, 35);
+	v.push_back(x7);
+	EXPECT_EQ(solucionFuerzaBruta(v, 25), 35);
+	v.pop_back();
 }
