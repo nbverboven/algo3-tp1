47f2a1adca36e9041b3a34a760fd7ca7de839b31
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-04-06 08:47:45 -0300

    Agregué casos de test para backtracking

--- tests.cpp
+++ tests.cpp
@@ -44,3 +44,110 @@ TEST(TestFuerzaBruta, HaySolucion)
 	EXPECT_EQ(solucionFuerzaBruta(v, 25), 35);
 	v.pop_back();
 }
+
+
+TEST(TestBacktrackingFactibilidad, VectorVacio)
+{
+	vector<pair<int,int>> v = {};
+
+	EXPECT_EQ(solucionBacktracking1(v, 25), 0);
+}
+
+TEST(TestBacktrackingFactibilidad, NoHaySolucion)
+{
+	pair<int,int> x1(3, 3);
+	pair<int,int> x2(4, 4);
+	pair<int,int> x3(5, 5);
+	vector<pair<int,int>> v = {x1, x2, x3};
+
+	EXPECT_EQ(solucionBacktracking1(v, 2), 0);
+}
+
+TEST(TestBacktrackingFactibilidad, HaySolucion)
+{
+	pair<int,int> x5(6, 8);
+	pair<int,int> x3(5, 10);
+	pair<int,int> x4(7, 7);
+	pair<int,int> x2(4, 1);
+	pair<int,int> x1(3, 9);
+	vector<pair<int,int>> v = {x5, x3, x4, x2, x1};
+
+	EXPECT_EQ(solucionBacktracking1(v, 7), 10);
+	EXPECT_EQ(solucionBacktracking1(v, 18), 28);
+	EXPECT_EQ(solucionBacktracking1(v, 25), 35);
+
+	pair<int,int> x6(18,28);
+	v.push_back(x6);
+	EXPECT_EQ(solucionBacktracking1(v, 18), 28);
+	v.pop_back();
+
+	pair<int,int> x7(25, 35);
+	v.push_back(x7);
+	EXPECT_EQ(solucionBacktracking1(v, 25), 35);
+	v.pop_back();
+}
+
+
+TEST(TestBacktrackingFactibilidad, OrdenCrecienteDeValor)
+{
+	pair<int,int> x5(6, 8);
+	pair<int,int> x3(5, 10);
+	pair<int,int> x4(7, 7);
+	pair<int,int> x2(4, 1);
+	pair<int,int> x1(3, 9);
+	vector<pair<int,int>> v = {x5, x3, x4, x2, x1};
+
+	sort(v.begin(), v.end());
+
+	EXPECT_EQ(solucionBacktracking1(v, 7), 10);
+	EXPECT_EQ(solucionBacktracking1(v, 18), 28);
+	EXPECT_EQ(solucionBacktracking1(v, 25), 35);
+
+	pair<int,int> x6(18,28);
+	v.push_back(x6);
+	EXPECT_EQ(solucionBacktracking1(v, 18), 28);
+	v.pop_back();
+
+	sort(v.begin(), v.end());
+
+	pair<int,int> x7(25, 35);
+	v.push_back(x7);
+	EXPECT_EQ(solucionBacktracking1(v, 25), 35);
+	v.pop_back();
+
+	sort(v.begin(), v.end());
+}
+
+
+TEST(TestBacktrackingFactibilidad, OrdenDerecienteDeValor)
+{
+	pair<int,int> x5(6, 8);
+	pair<int,int> x3(5, 10);
+	pair<int,int> x4(7, 7);
+	pair<int,int> x2(4, 1);
+	pair<int,int> x1(3, 9);
+	vector<pair<int,int>> v = {x5, x3, x4, x2, x1};
+
+	sort(v.begin(), v.end());
+	reverse(v.begin(), v.end());
+
+	EXPECT_EQ(solucionBacktracking1(v, 7), 10);
+	EXPECT_EQ(solucionBacktracking1(v, 18), 28);
+	EXPECT_EQ(solucionBacktracking1(v, 25), 35);
+
+	pair<int,int> x6(18,28);
+	v.push_back(x6);
+	EXPECT_EQ(solucionBacktracking1(v, 18), 28);
+	v.pop_back();
+
+	sort(v.begin(), v.end());
+	reverse(v.begin(), v.end());
+
+	pair<int,int> x7(25, 35);
+	v.push_back(x7);
+	EXPECT_EQ(solucionBacktracking1(v, 25), 35);
+	v.pop_back();
+
+	sort(v.begin(), v.end());
+	reverse(v.begin(), v.end());
+}
