52dcdcb5f5ff5fc3bfe099fe598a39d1cbdc1805
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-04-11 00:27:16 -0300

    Primer experimento

--- grafico.py
+++ grafico.py
@@ -6,9 +6,13 @@ inputfile = sys.argv[1]
 
 datos = np.loadtxt(inputfile, delimiter=',')
 
-plt.plot(datos[:,0], datos[:,1], 'bo', label='Fuerza bruta', linewidth=2)
-plt.plot(datos[:,0], datos[:,1], 'g^', label='Fuerza bruta2', linewidth=2)
-#plt.yscale('log')
+plt.plot(datos[:,0], datos[:,1], label='Fuerza bruta', linewidth=2)
+plt.plot(datos[:,0], datos[:,2], label='Backtracking', linewidth=2)
+plt.plot(datos[:,0], datos[:,3], label='Programación dinámica', linewidth=2)
+plt.plot(datos[:,0], datos[:,4], label='Fuerza bruta2', linewidth=2)
+plt.plot(datos[:,0], datos[:,5], label='Backtracking2', linewidth=2)
+plt.plot(datos[:,0], datos[:,6], label='Programación dinámica2', linewidth=2)
+plt.yscale('log')
 plt.xlabel('Cantidad de elementos', color='black')
 plt.ylabel('Tiempo de ejecucion (ms)', color= 'black')
 plt.legend(loc=2)
--- tiempos.cpp
+++ tiempos.cpp
@@ -4,6 +4,9 @@
 #include <random>
 #include "algoritmos.h"
 
+#define TAM_FUERZA_BRUTA 80
+#define TAM_BACKTRACKING 80
+
 using namespace std;
 
 int sumaDeCostos(const vector<pair<int,int>> &v)
@@ -29,7 +32,7 @@ int sumaDeMediosCostos(const vector<pair<int,int>> &v)
 }
 
 
-/* EXPERIMENTO FUERZA BRUTA 1
+/* EXPERIMENTO  1
    * # muestras: 30
    * Tamaño del vector: a determinar
    * Costos al azar entre 1 y 100
@@ -37,28 +40,37 @@ int sumaDeMediosCostos(const vector<pair<int,int>> &v)
    * Capacidad máxima: 2*(suma de todos los costos) y
                        suma de la mitad de todos los costos
 */
-void experimentoFuerzaBruta1(int max_size, int granularity)
+void experimento1(int max_size, int granularity)
 {
-	// Acá voy a guardar el tiempo promedio que tarda el algoritmo
-	// para cada tamaño de vector
-	vector<double> resultados;
-	vector<double> resultados2;
+	/* 
+	   Acá voy a guardar el tiempo promedio que tarda el algoritmo
+	   para cada tamaño de vector.
+	   Indices:
+	   	- 0 y 3: fuerza bruta
+	   	- 1 y 4: backtracking
+	   	- 2 y 5: programación dinámica
+	   Capacidades máximas:
+	    - 0, 1 y 2: 2*(suma de todos los costos)
+	    - 3, 4,y 5: suma de la mitad de todos los costos
+	*/
+
+	vector<double> resultados[6];
 
 	std::random_device rd; // obtain a random number from hardware
 	std::mt19937 eng(rd()); // seed the generator
 	std::uniform_int_distribution<> distr(1, 100); // define the range
 
-
 	for (int i = 1; i < max_size+1; i+=granularity)
 	{
-
-		vector<double> resultados_parciales;
-		vector<double> resultados_parciales2;
+		// La convención de índices es la misma que para resultados
+		vector<double> resultados_parciales[6];
 
 		// Para cada longitud del vector, tomo 30 muestras
 		for (int j = 0; j < 30; ++j)
 		{
-			// Genero el vector de tamaño i de pares al azar
+			/* Genero el vector de tamaño i de pares al azar.
+			   Todos los algoritmos los aplico siempre sobre el
+			   mismo vector, para poder comparar los tiempos.*/
 			vector<pair<int,int>> v;
 			for (int k = 0; k < i; ++k)
 			{
@@ -69,48 +81,134 @@ void experimentoFuerzaBruta1(int max_size, int granularity)
 			// Para calcular la capacidad máxima
 			int sum_w = sumaDeCostos(v);
 
-			// Corro el algoritmo de fuerza bruta y veo cuánto tarda
-			auto startFuerzaBruta = chrono::steady_clock::now();
-			solucionFuerzaBruta(v, 2*sum_w);
-			auto endFuerzaBruta = chrono::steady_clock::now();
-			auto diffFuerzaBruta = endFuerzaBruta - startFuerzaBruta;
-			resultados_parciales.push_back(chrono::duration <double, milli> (diffFuerzaBruta).count());
+			if (i <= TAM_FUERZA_BRUTA)
+			{
+				/* Corro el algoritmo de fuerza bruta solo para tamaños chicos.
+				   Si no, tarda demasiado */
+				auto startFuerzaBruta = chrono::steady_clock::now();
+				solucionFuerzaBruta(v, 2*sum_w);
+				auto endFuerzaBruta = chrono::steady_clock::now();
+				auto diffFuerzaBruta = endFuerzaBruta - startFuerzaBruta;
+				resultados_parciales[0].push_back(chrono::duration <double, milli> (diffFuerzaBruta).count());
+			}
+			else
+			{
+				resultados_parciales[0].push_back(0.0);
+			}
+
+			// Después veo si tengo que cambiar este número
+			if (i <= TAM_BACKTRACKING)
+			{
+				auto startBacktracking = chrono::steady_clock::now();
+				solucionBacktracking(v, 2*sum_w);
+				auto endBacktracking = chrono::steady_clock::now();
+				auto diffBacktracking = endBacktracking - startBacktracking;
+				resultados_parciales[1].push_back(chrono::duration <double, milli> (diffBacktracking).count());
+			}
+			else
+			{
+				resultados_parciales[1].push_back(0.0);
+			}
+
+
+			auto startProgDinamica = chrono::steady_clock::now();
+			solucionProgDinamica(v, 2*sum_w);
+			auto endProgDinamica = chrono::steady_clock::now();
+			auto diffProgDinamica = endProgDinamica - startProgDinamica;
+			resultados_parciales[2].push_back(chrono::duration <double, milli> (diffProgDinamica).count());
+
+
 
 			// Realizo el experimento con el otro costo máximo
 			int sum_mitad_w = sumaDeMediosCostos(v);
 
-			auto startFuerzaBruta2 = chrono::steady_clock::now();
-			solucionFuerzaBruta(v, sum_mitad_w);
-			auto endFuerzaBruta2 = chrono::steady_clock::now();
-			auto diffFuerzaBruta2 = endFuerzaBruta2 - startFuerzaBruta2;
-			resultados_parciales2.push_back(chrono::duration <double, milli> (diffFuerzaBruta2).count());
+			if (i <= 20)
+			{
+				/* Corro el algoritmo de fuerza bruta solo para tamaños chicos.
+				   Si no, tarda demasiado */
+				auto startFuerzaBruta2 = chrono::steady_clock::now();
+				solucionFuerzaBruta(v, 2*sum_mitad_w);
+				auto endFuerzaBruta2 = chrono::steady_clock::now();
+				auto diffFuerzaBruta2 = endFuerzaBruta2 - startFuerzaBruta2;
+				resultados_parciales[3].push_back(chrono::duration <double, milli> (diffFuerzaBruta2).count());
+			}
+			else
+			{
+				resultados_parciales[3].push_back(0.0);
+			}
+
+			// Después veo si tengo que cambiar este número
+			if (i <= 25)
+			{
+				auto startBacktracking2 = chrono::steady_clock::now();
+				solucionBacktracking(v, 2*sum_mitad_w);
+				auto endBacktracking2 = chrono::steady_clock::now();
+				auto diffBacktracking2 = endBacktracking2 - startBacktracking2;
+				resultados_parciales[4].push_back(chrono::duration <double, milli> (diffBacktracking2).count());
+			}
+			else
+			{
+				resultados_parciales[4].push_back(0.0);
+			}
+
+
+			auto startProgDinamica2 = chrono::steady_clock::now();
+			solucionProgDinamica(v, 2*sum_mitad_w);
+			auto endProgDinamica2 = chrono::steady_clock::now();
+			auto diffProgDinamica2 = endProgDinamica2 - startProgDinamica2;
+			resultados_parciales[5].push_back(chrono::duration <double, milli> (diffProgDinamica2).count());
 		}
 
-		// Calculo el promedio de tiempos y me lo guardo
-		double prom = resultados_parciales[0];
-		double prom2 = resultados_parciales2[0];
-		for (unsigned int i = 1; i < resultados_parciales.size(); ++i)
+		// Calculo los promedios de tiempos y me los guardo
+		double proms[6];
+		for (int i = 0; i < 6; ++i)
 		{
-			prom += resultados_parciales[i];
-			prom2 += resultados_parciales2[i];
+			proms[i] = resultados_parciales[i][0];
 		}
 
-		resultados.push_back(prom/resultados_parciales.size());
-		resultados2.push_back(prom/resultados_parciales2.size());
+		//fb
+		for (unsigned int i = 1; i < resultados_parciales[0].size(); ++i)
+		{
+			proms[0] += resultados_parciales[0][i];
+			proms[3] += resultados_parciales[3][i];
+		}
+
+		//bcktrck
+		for (unsigned int i = 1; i < resultados_parciales[1].size(); ++i)
+		{
+			proms[1] += resultados_parciales[1][i];
+			proms[4] += resultados_parciales[4][i];
+		}
+
+		//pd
+		for (unsigned int i = 1; i < resultados_parciales[2].size(); ++i)
+		{
+			proms[2] += resultados_parciales[2][i];
+			proms[5] += resultados_parciales[5][i];
+		}
+
+		for (int i = 0; i < 6; ++i)
+		{
+			resultados[i].push_back(proms[i]/resultados_parciales[i].size());
+		}
 	}
 	
-	// Guardo los resultados en un archivo de tessto
+	// Guardo los resultados en un archivo de texto
 	ofstream outfile;
-	outfile.open("exp_fb_1.csv", ios::out);
+	outfile.open("exp_1.csv", ios::out);
 
 	// Escribo todos los resultados en el archivo
-	outfile << "0,0.0,0.0"  << endl;
+	outfile << "0,0.0,0.0,0.0,0.0,0.0,0.0"  << endl;
 	int asd = 0;
-	for (unsigned int i = 0; i < resultados.size(); ++i)
+	for (unsigned int i = 0; i < resultados[2].size(); ++i)
 	{
-		outfile << asd+granularity << "," << resultados[i] 
-		                           << "," << resultados2[i] 
-		                           << endl;
+		outfile << asd+granularity << ","
+			    << ((i <= TAM_FUERZA_BRUTA) ? resultados[0][i] : 0.0) << ","
+			    << ((i <= TAM_BACKTRACKING) ? resultados[1][i] : 0.0) << ","
+			    << resultados[2][i] << ","
+			    << ((i <= TAM_FUERZA_BRUTA) ? resultados[3][i] : 0.0) << ","
+			    << ((i <= TAM_BACKTRACKING) ? resultados[4][i] : 0.0) << ","
+			    << resultados[5][i] << endl;
 		asd += granularity;
 	}
 
@@ -121,14 +219,7 @@ void experimentoFuerzaBruta1(int max_size, int granularity)
 
 int main()
 {
-	// cout << endl << endl;
-	// auto startBacktracking = chrono::steady_clock::now();
-	// solucionBacktracking1(v, 25);
-	// auto endBacktracking = chrono::steady_clock::now();
-	// auto diffBacktracking = endBacktracking - startBacktracking;
-	// cout << "Tiempo utilizado por Backtracking " << chrono::duration <double, milli> (diffBacktracking).count() << " ms" << endl;
-
-	experimentoFuerzaBruta1(30, 5);
+	experimento1(50, 1);
 
 	return 0;
 }
