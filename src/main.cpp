e1b5a390b3dd53fd9b6a093e6c1cc468b0b9b407
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-04-14 17:36:55 -0300

    Agregado el código para que haga lo que pedía el enunciado

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <utility>
 #include "algoritmos.h"
 
-using namespace std;
-
-int main(int argc, char const *argv[])
+int main()
 {
-	/* code */
+	int cant_elem;
+	int capac_max;
+	std::vector<std::pair<int,int>> v;
+
+	/* Leo desde el standard input la cantidad de
+	   elementos que tendrá el vector y la capacidad
+	   máxima de la mochila */
+	std::cin >> cant_elem;
+	std::cin >> capac_max;
+
+	/* Leo los elementos y los agrego de a uno al vector */
+	while (cant_elem > 0)
+	{
+		std::pair<int,int> e;
+		std::cin >> e.first;
+		std::cin >> e.second;
+		v.push_back(e);
+		--cant_elem;
+	}
+
+	/* Descomentar el algoritmo que se prefiera
+	   y comentar el resto */
+	// std::cout << solucionFuerzaBruta(v, e);
+	// std::cout << solucionBacktracking(v, e);
+	std::cout << solucionProgDinamica(v, capac_max) << std::endl;
+
 	return 0;
 }
